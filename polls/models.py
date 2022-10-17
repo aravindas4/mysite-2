61ee7cad937841a2900c19cefcaab25222b8063a
aravindas4 <ash.g.education@gmail.com>
2022-10-17 15:58:39 +0530

    Add changes

--- polls/models.py
+++ polls/models.py
@@ -34,6 +34,32 @@ class Choice(models.Model):
         return self.choice_text
 
 
+class CatalogBackup(models.Model):
+    biz = models.ForeignKey(
+        Choice, related_name='catalog_backups', on_delete=models.CASCADE
+    )
+    updated_by = models.ForeignKey(
+        Question, related_name="my_catalog_backups", blank=True, null=True,
+        on_delete=models.SET_NULL
+    )
+    title = models.CharField("Title", max_length=255)
+    description = models.TextField(
+        "Description", blank=True, null=True, default=None
+    )
+    created = models.DateTimeField(auto_now=False, auto_now_add=True)
+    backup_file = models.FileField(
+        "Backup file", upload_to='catalog_backups/l/%Y/%m/%d',
+        blank=True, null=True, default=None
+    )
+
+    metric_details = models.TextField(
+        "Metric details", blank=True, null=True, default="{}"
+    )
+
+    class Meta:
+        db_table = "atlas_catalogbackup"
+
+
 @receiver(post_save, sender=Question)
 def post_signal_for_question(sender, instance, **kwargs):
     pass 
